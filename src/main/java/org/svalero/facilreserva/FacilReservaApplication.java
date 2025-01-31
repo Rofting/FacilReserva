df717f5c867673424c6d81d93322dda58bb0d941
Jose <a27941@svalero.com>
2025-01-31 10:01:51 +0100

    Creamos el proyecto

---
+++ src/main/java/org/svalero/facilreserva/FacilReservaApplication.java
@@ -0,0 +1,13 @@
+package org.svalero.facilreserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilReservaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/svalero/facilreserva/FacilReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.svalero.facilreserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilReservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
