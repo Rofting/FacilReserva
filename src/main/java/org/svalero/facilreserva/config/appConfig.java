f311a566e2e2724f0c05e70d0901bce403c31ef8
Jorge <a22609@svalero.com>
2025-01-31 12:06:01 +0100

    agregamos la carpeta config

---
+++ src/main/java/org/svalero/facilreserva/config/appConfig.java
@@ -0,0 +1,15 @@
+package org.svalero.facilreserva.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class appConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
