e4121803e729c7146f8bc8663540e3683dc044c5
Jose <a27941@svalero.com>
2025-01-31 10:40:28 +0100

    Agregamos el codigo de la clases restaurant y reservation

--- src/main/java/org/svalero/facilreserva/domain/Reservation.java
+++ src/main/java/org/svalero/facilreserva/domain/Reservation.java
@@ -1,4 +1,27 @@
 package org.svalero.facilreserva.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String nameReservation;
+
+    @Column(nullable = false)
+    private String dateReservation;
+
+    @Column(nullable = false)
+    private int persons;
+
+    @Column(nullable = false)
+    private Boolean confirmation;
+
 }
--- src/main/java/org/svalero/facilreserva/domain/Restaurant.java
+++ src/main/java/org/svalero/facilreserva/domain/Restaurant.java
@@ -1,4 +1,26 @@
 package org.svalero.facilreserva.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique=true)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String capacity;
+
+    @Column(nullable = false)
+    private Boolean available;
+
 }
